bd31cc4e74bbc9d03a8cf1ed499c567eae9ce8b0
Azimkhoja <ahmadkhonovazimkhoja@.com>
2023-04-09 10:44:51 +0500

    ok

---
+++ src/words/medium.js
@@ -0,0 +1,16 @@
+const medium = {
+    en: {
+        alive:1,
+        aloud:2,
+        already:3,
+        allow:4,
+        achieve:5,
+        amazing:6,
+        afford:7,
+        blame:8,
+        belong:9,
+        benefit:10,
+        blow:11,
+        
+    }
+}
